5cb56d7dc8c18482a2e83aeeef5b186ac152810d
melukas <l.kiffmeyer@lvm.de>
2018-06-07 12:39:57 +0200

    Dokumentation

--- Befehlsdetail.java
+++ Befehlsdetail.java
@@ -1,6 +1,13 @@
- public enum Befehlsdetail {
+/**
+ * Darstellung von Möglichen Befehlsdetails von Befehlen
+ */
+public enum Befehlsdetail {
 	oben, unten, sueden, norden, osten, westen;
 	
+	/**
+	 * Gibt zu einem gegebenen Befehlsdetail (explizit Himmelsrichtungen)
+	 * das gegenteilige Befehlsdetail.
+	 */
 	public static Befehlsdetail getGegenteil(Befehlsdetail detail) {
 		switch(detail) {
 		case oben: return unten;
